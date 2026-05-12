7838939d62b401421515edff96f07377f7780a0a
vvv347 <vvv347@gmail.com>
2026-05-12 05:55:58 -0700

    My first commit

--- HelloApp/ContentView.swift
+++ HelloApp/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Привет, мир! 🌍")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
         }
         .padding()
     }
