704caa8d83782f5a25b3a67ab818b9030e67e448
vvv347 <vvv347@gmail.com>
2026-05-12 05:28:56 -0700

    Initial Commit

---
+++ HelloApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloApp
+//
+//  Created by Владимир Шман on 12.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by Владимир Шман on 12.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
